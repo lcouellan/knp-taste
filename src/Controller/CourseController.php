f3a53c0424f2a977b368908fadc65764dc2af747
Lénaïc <lcouellan@gmail.com>
2018-08-27 17:23:55 +0200

    Removing route annotations from Controller

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -5,13 +5,9 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CourseController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage");
-     */
     public function homepageAction()
     {
         return $this->render('course/homepage.html.twig');
