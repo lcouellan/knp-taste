cc335b4328b9f41f6de37908a1e49ddb79a6329c
Lénaïc <lcouellan@gmail.com>
2018-08-28 13:56:44 +0200

    Setting up signup

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -9,14 +9,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CourseController extends AbstractController
 {
+
+    /**
+     * Homepage of the application
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function homepageAction()
     {
         return $this->render('course/homepage.html.twig');
     }
 
+    /**
+     * Page for choose which course to watch
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function viewAllCourses()
     {
-
         $courses = $this->getDoctrine()->getRepository(Course::class)->findAll();
 
         return $this->render('course/show_all.html.twig', [
@@ -24,10 +34,17 @@ class CourseController extends AbstractController
         ]);
     }
 
+    /**
+     * Page for watch the course
+     *
+     * @param $id : id of the course
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function viewCourse($id)
     {
         $course = $this->getDoctrine()->getRepository(Course::class)->find($id);
 
+        // If the course is not find, redirect the user to the homepage
         if(!$course instanceof Course) {
             $session = $this->container->get('session');
             $session->getFlashBag()->add('error', 'This course doesn\'t exist');
---
+++ src/Controller/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Course;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validation;
+
+class UserController extends AbstractController
+{
+    public function signupAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('validate', SubmitType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        $formView = $form->createView();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+
+            //TODO: Fix with the rad user
+            $user->setPassword('aaa');
+
+
+            // Validate the user
+            $validator = Validation::createValidator();
+            $errors = $validator->validate($user);
+
+            $userSameEmail = $this
+                ->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(['email' => $data['email']])
+            ;
+            $userSameUsername = $this
+                ->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(['username' => $data['username']])
+            ;
+
+
+            if (count($errors) > 0 || $userSameEmail instanceof User || $userSameUsername instanceof User) {
+
+                $errorsString = (string) $errors;
+
+                return $this->render('user/signup.html.twig', [
+                    'form' => $formView,
+                    'errors' => $errorsString
+                ]);
+            }
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+
+            //TODO: Connect the user
+
+            // Redirect the user to the homepage
+            $session = $this->container->get('session');
+            $session->getFlashBag()->add('success', 'You are now connected');
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('user/signup.html.twig', [
+            'form' => $formView
+        ]);
+    }
+}
