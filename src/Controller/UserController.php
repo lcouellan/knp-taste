65d349e3bef0b87a802c4149d4e0f833e99815aa
Lénaïc <lcouellan@gmail.com>
2018-08-29 09:23:26 +0200

    Updating subscribe form component with FormType

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Course;
 use App\Entity\User;
+use App\Form\Type\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -26,57 +27,14 @@ class UserController extends AbstractController
      */
     public function signupAction(Request $request)
     {
-        $form = $this->createFormBuilder()
-            ->add('username', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'The password fields must match',
-                'required' => true,
-                'first_options' => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat password']
-            ])
-            ->add('validate', SubmitType::class)
-            ->getForm()
-        ;
+        $form = $this->createForm(RegisterFormType::class);
 
         $form->handleRequest($request);
 
         $formView = $form->createView();
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $user = new User();
-            $user->setUsername($data['username']);
-            $user->setEmail($data['email']);
-            $user->setPlainPassword($data['password']);
-
-
-            // Validate the user
-            $validator = Validation::createValidator();
-            $errors = $validator->validate($user);
-
-            $userSameEmail = $this
-                ->getDoctrine()
-                ->getRepository(User::class)
-                ->findOneBy(['email' => $data['email']])
-            ;
-            $userSameUsername = $this
-                ->getDoctrine()
-                ->getRepository(User::class)
-                ->findOneBy(['username' => $data['username']])
-            ;
-
-            // User is not validated, error displayed
-            if (count($errors) > 0 || $userSameEmail instanceof User || $userSameUsername instanceof User) {
-
-                $errorsString = (string) $errors;
-
-                return $this->render('user/signup.html.twig', [
-                    'form' => $formView,
-                    'errors' => $errorsString
-                ]);
-            }
+            $user = $form->getData();
 
             $manager = $this->getDoctrine()->getManager();
             $manager->persist($user);
